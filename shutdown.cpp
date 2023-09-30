4b8a94a3329cf4420cbcd1ffdf92a3c4d1c3713f
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2023-09-30 15:28:13 +0100

    Create shutdown.cpp

---
+++ shutdown.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    char choice;
+    string choice_2;
+    cout<<"do you want restart or shutdown for restart type r for shutdown type s"<<endl;
+    cin>>choice;
+    jump:
+if(choice == 'r' || choice == 'R'){
+    system("shutdown -r -t 0");
+    system("reboot");
+    system("c:\windows\system32\shutdown /r ");
+}
+if(choice == 's' || choice == 'S'){
+    system("poweroff");
+    system("shutdown -P now");
+    system("c:\windows\system32\shutdown /i ");
+}
+else{
+    cout<<"wrong input"<<'\n'<<"do you want to try again?(yes/no)"<<endl;
+    cin>>choice_2;
+}
+if(choice_2 == "yes" || choice_2 == "Yes"){
+    goto jump;
+}
+else{
+    cout<<"end of program"<<endl;
+}
+    return 0;
+}
