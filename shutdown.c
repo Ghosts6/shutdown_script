3bbc15d848f7e73bcef2d2de805122e6e24703d4
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-03-20 11:47:58 +0000

    Update shutdown.c

--- shutdown.c
+++ shutdown.c
@@ -1,6 +1,45 @@
-#include <stio.h>
+#include <stdio.h>
 #include <stdlib.h>
-int main(){
-	system("c:\\Windows\\System32\\shutdown /s")
-	return 0;
+#include <string.h>
+
+void shutdown();
+void restart();
+
+int main() {
+    char choice[20]; 
+    label:
+    printf("Select operation type (restart/shutdown) \n"); 
+    scanf("%s", choice); 
+    if (strcmp(choice, "restart") == 0 || strcmp(choice, "Restart") == 0) {
+        restart();
+    }
+    else if (strcmp(choice, "shutdown") == 0 || strcmp(choice, "Shutdown") == 0) { 
+        shutdown();
+    }
+    else {
+        printf("wrong choice please try again \n");
+        goto label;
+    }
+    printf("End of program\n");
+    return 0;
+}
+
+void shutdown(){
+    #ifdef _WIN32
+        system("shutdown /s");
+    #elif __unix__
+        system("sudo poweroff");
+    #elif __linux__
+        system("sudo poweroff");
+    #endif
+}
+
+void restart(){
+    #ifdef _WIN32
+        system("shutdown /r");
+    #elif __unix__
+        system("sudo reboot");
+    #elif __linux__
+        system("sudo reboot");
+    #endif
 }
